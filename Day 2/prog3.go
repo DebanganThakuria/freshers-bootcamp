aa866d297b496efaa519bd9d6a0cec2ab1217ef8
DebanganThakuria <debanganthakuria44@gmail.com>
2021-07-22 21:29:46 +0530

    Uses Waitgroup

--- Day 2/prog3.go
+++ Day 2/prog3.go
@@ -3,22 +3,24 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 type account struct {
 	balance int
 }
 
-func (a *account) deposit(amount int, m *sync.Mutex) {
+func (a *account) deposit(amount int, m *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
 	m.Lock()
 	a.balance += amount
 	m.Unlock()
 }
 
-func (a *account) withdraw(amount int, m *sync.Mutex) {
+func (a *account) withdraw(amount int, m *sync.Mutex, wg *sync.WaitGroup) {
 	m.Lock()
+	defer wg.Done()
 	defer m.Unlock()
+
 	if amount > a.balance {
 		fmt.Println("Not enough balance")
 		return
@@ -28,18 +30,21 @@ func (a *account) withdraw(amount int, m *sync.Mutex) {
 
 func main() {
 	var m1 sync.Mutex
+	var wg sync.WaitGroup
 
 	a1 := account{500}
 
 	for i := 0; i < 10; i++ {
-		go a1.deposit(50, &m1)
+		wg.Add(1)
+		go a1.deposit(50, &m1, &wg)
 	}
 
 
 	for i := 0; i < 10; i++ {
-		go a1.withdraw(100, &m1)
+		wg.Add(1)
+		go a1.withdraw(100, &m1, &wg)
 	}
 
-	time.Sleep(2 * time.Second)
+	wg.Wait()
 	fmt.Println(a1.balance)
 }
